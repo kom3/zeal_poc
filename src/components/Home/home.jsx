f0c0d6412cf42012baf53d0d41a0be30fdb1e6cb
kom3 <manjunathkankatteko@gmail.com>
2023-11-05 19:32:44 +0530

    .

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,18 +1,17 @@
 import Footer from "../Footer/footer";
 import Header from "../Header/header";
+import { LineChart } from "../charts/lineChart/lineChart";
+import "./home.scss";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        Welcome home...!
+
+      <div className="body_wrpr">
+        <LineChart />
       </div>
+
       <Footer />
     </div>
   );
